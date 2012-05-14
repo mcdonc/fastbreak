f181f19664d2011e668818ebf9ffefa3ce166d2b
Paul Everitt <paul@agendaless.com>
2012-05-14 13:55:18 -0400

    Players have guardian with Adults.

--- fastbreak/player/__init__.py
+++ fastbreak/player/__init__.py
@@ -10,15 +10,33 @@ from substanced.service import find_service
 from fastbreak.interfaces import (
     IPlayer,
     ITeam,
+    IAdult
     )
-from fastbreak.utils import PLAYERTOTEAM
+from fastbreak.utils import (
+    PLAYERTOTEAM,
+    PLAYERTOPRIMARYGUARDIAN
+    )
 
 
 @colander.deferred
-def teams_widget(node, kw):
+def team_widget(node, kw):
     request = kw['request']
     search_catalog = request.search_catalog
     count, oids, resolver = search_catalog(interfaces=(ITeam,))
+    values = []
+    for oid in oids:
+        title = resolver(oid).title
+        values.append(
+            (str(oid), title)
+        )
+    return ChosenSingleWidget(values=values)
+
+
+@colander.deferred
+def guardian_widget(node, kw):
+    request = kw['request']
+    search_catalog = request.search_catalog
+    count, oids, resolver = search_catalog(interfaces=(IAdult,))
     values = []
     for oid in oids:
         title = resolver(oid).title
@@ -34,7 +52,12 @@ class PlayerSchema(Schema):
     )
     team = colander.SchemaNode(
         colander.Int(),
-        widget=teams_widget,
+        widget=team_widget,
+        missing=colander.null
+    )
+    primary_guardian = colander.SchemaNode(
+        colander.Int(),
+        widget=guardian_widget,
         missing=colander.null
     )
 
@@ -56,10 +79,18 @@ class PlayerBasicPropertySheet(PropertySheet):
         else:
             team = teams[0]
 
+        # Need the objectid of the primary guardian
+        primary_guardians = list(context.get_primary_guardianids())
+        if not primary_guardians:
+            primary_guardian = colander.null
+        else:
+            primary_guardian = primary_guardians[0]
+
         return dict(
             name=context.__name__,
             title=context.title,
-            team=team
+            team=team,
+            primary_guardian=primary_guardian
         )
 
     def set(self, struct):
@@ -68,7 +99,8 @@ class PlayerBasicPropertySheet(PropertySheet):
 
         # Disconnect old relations, make new relations
         context.disconnect()
-        context.connect(struct['team'])
+        context.connect_team(struct['team'])
+        context.connect_primary_guardian(struct['primary_guardian'])
 
 
 @content(
@@ -82,7 +114,7 @@ class PlayerBasicPropertySheet(PropertySheet):
     catalog=True,
     )
 class Player(Persistent):
-    def __init__(self, title, team):
+    def __init__(self, title, team, primary_guardian):
         self.title = title
         # We don't care about storing team
 
@@ -93,16 +125,29 @@ class Player(Persistent):
         objectmap = find_service(self, 'objectmap')
         return objectmap.targetids(self, PLAYERTOTEAM)
 
-    def connect(self, *teams):
+    def get_primary_guardianids(self):
+        objectmap = find_service(self, 'objectmap')
+        return objectmap.targetids(self, PLAYERTOPRIMARYGUARDIAN)
+
+    def connect_team(self, *teams):
         objectmap = find_service(self, 'objectmap')
         for teamid in teams:
             objectmap.connect(self, teamid, PLAYERTOTEAM)
 
+    def connect_primary_guardian(self, *primary_guardian):
+        objectmap = find_service(self, 'objectmap')
+        for adultid in primary_guardian:
+            objectmap.connect(self, adultid, PLAYERTOPRIMARYGUARDIAN)
+
     def disconnect(self):
         teams = self.get_teamids()
+        primary_guardian = self.get_primary_guardianids()
         objectmap = find_service(self, 'objectmap')
         for teamid in teams:
             objectmap.disconnect(self, teamid, PLAYERTOTEAM)
+        for adultid in primary_guardian:
+            objectmap.disconnect(self, adultid,
+                                 PLAYERTOPRIMARYGUARDIAN)
 
     def teams(self):
         objectmap = find_service(self, 'objectmap')
--- fastbreak/utils.py
+++ fastbreak/utils.py
@@ -2,8 +2,8 @@
 # Constants
 DOCUMENTTOTEAM = 'document-to-team'
 PLAYERTOTEAM = 'player-to-team'
-PLAYERTOGUARDIAN1 = 'player-to-guardian1'
-PLAYERTOGUARDIAN2 = 'player-to-guardian2'
+PLAYERTOPRIMARYGUARDIAN = 'player-to-primaryguardian'
+PLAYERTOOTHERGUARDIAN = 'player-to-otherguardian'
 HEADCOACHTTOTEAM = 'headcoach-to-team'
 ASSISTANTCOACHTTOTEAM = 'assistantcoach-to-team'
 MANAGERTOTEAM = 'manager-to-team'
