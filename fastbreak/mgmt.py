e73c9540ed9067006481143e34fde867efc746ab
Paul Everitt <paul@agendaless.com>
2012-05-14 14:59:22 -0400

    Sample data with relations

--- fastbreak/mgmt.py
+++ fastbreak/mgmt.py
@@ -3,20 +3,29 @@ from pyramid.httpexceptions import HTTPFound
 from substanced.form import FormView
 from substanced.schema import Schema
 from substanced.sdi import mgmt_view
+from substanced.service import find_service
 from substanced.site import ISite
 
-from fastbreak.utils import make_name
+from fastbreak.utils import (
+    make_name,
+    sample_data
+    )
 
 from fastbreak.interfaces import (
     ITeam,
     IDocument,
-    IProgram
+    IProgram,
+    IAdult,
+    IPlayer
     )
 from fastbreak.resources import (
     DocumentSchema,
     DocumentBasicPropertySheet
     )
+from fastbreak.adult import AdultBasicPropertySheet
+from fastbreak.player import PlayerBasicPropertySheet
 from fastbreak.team import TeamBasicPropertySheet
+
 
 @mgmt_view(
     context=IProgram,
@@ -40,6 +49,7 @@ class AddDocumentView(FormView):
         propsheet.set(appstruct)
         return HTTPFound(self.request.mgmt_path(document, '@@properties'))
 
+
 @mgmt_view(
     context=ISite,
     name='import_data',
@@ -55,8 +65,10 @@ class ImportDataView(FormView):
     def import_success(self, appstruct):
         root = self.request.root
         registry = self.request.registry
+        objectmap = find_service(self.context, 'objectmap')
 
-        # First add STORM as a program
+
+    # First add STORM as a program
         name = u'storm'
         appstruct = dict(title='STORM')
         storm = registry.content.create(IProgram, **appstruct)
@@ -64,15 +76,58 @@ class ImportDataView(FormView):
         propsheet = TeamBasicPropertySheet(storm, self.request)
         propsheet.set(appstruct)
 
-        # Add some Teams
-        teams = (u'Blue', u'Orange', u'White', u'Black', u'Silver')
-        for title in teams:
-            name = make_name(title)
-            appstruct = dict(title=title)
+        # Add some Teams and families
+        for team_title, families in sample_data.items():
+            # First make a team
+            name = make_name(team_title)
+            appstruct = dict(title=team_title)
             team = registry.content.create(ITeam, **appstruct)
             storm[name] = team
             propsheet = TeamBasicPropertySheet(team, self.request)
             propsheet.set(appstruct)
+            team_oid = objectmap.objectid_for(team)
+
+            # Make values for each family
+            for family in families:
+                # Primary guardian
+                primary_title = family[0]
+                primary_name = make_name(primary_title)
+                appstruct = dict(title=primary_title)
+                primary_adult = registry.content.create(IAdult,
+                                                        **appstruct)
+                root[primary_name] = primary_adult
+                propsheet = AdultBasicPropertySheet(primary_adult,
+                                                    self.request)
+                propsheet.set(appstruct)
+                primary_oid = objectmap.objectid_for(primary_adult)
+
+                # Other guardian
+                other_title = family[1]
+                other_name = make_name(other_title)
+                appstruct = dict(title=other_title)
+                other_adult = registry.content.create(IAdult,
+                                                      **appstruct)
+                root[other_name] = other_adult
+                propsheet = AdultBasicPropertySheet(other_adult,
+                                                    self.request)
+                propsheet.set(appstruct)
+                other_oid = objectmap.objectid_for(other_adult)
+
+                # Player
+                player_title = family[2]
+                player_name = make_name(player_title)
+                appstruct = dict(
+                    title=player_title,
+                    team=team_oid,
+                    primary_guardian=primary_oid,
+                    other_guardian=other_oid
+                )
+                player = registry.content.create(IPlayer,
+                                                 **appstruct)
+                root[player_name] = player
+                propsheet = PlayerBasicPropertySheet(player,
+                                                     self.request)
+                propsheet.set(appstruct)
 
         return HTTPFound(self.request.mgmt_path(self.context,
                                                 '@@contents'))
--- fastbreak/utils.py
+++ fastbreak/utils.py
@@ -1,4 +1,3 @@
-
 # Constants
 DOCUMENTTOTEAM = 'document-to-team'
 PLAYERTOTEAM = 'player-to-team'
@@ -15,3 +14,16 @@ def make_name(title):
     name = title.replace(' ', '-').lower()
     return name
 
+white_familes = [
+    (u'Jim Hoover', u'Cheri Hoover', u'Peanut Hoover'),
+    (u'Paul Everitt', u'Herveline Everitt', u'Morgan Everitt'),
+    (u'Kristen Buechner', u'Chris Buechner', u'Kennedy Buechner')
+]
+orange_families = [
+    (u'Stacey Strobl', u'Mike Strobl', u'Olivia Strobl')
+]
+
+sample_data = dict(
+    Blue=[], White=white_families, Orange=orange_familes,
+    Black=[], Silver=[]
+)
