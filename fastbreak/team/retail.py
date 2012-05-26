c7440de3d63f9cdd1809d51e9b0a69dbbd907bf2
Paul Everitt <paul@agendaless.com>
2012-05-26 11:13:55 -0400

    Sync seems to work, added contact info

--- fastbreak/team/retail.py
+++ fastbreak/team/retail.py
@@ -24,16 +24,15 @@ class SplashView(Layout):
             dict(title='Contact Info',
                  url=resource_url(context, request, 'contact_info')),
             dict(title='Email Team',
-                 url=resource_url(context, request, 'email_team')),
+                 url=resource_url(context, request, 'emails')),
             ]
         return items
 
     @view_config(renderer='templates/team_view.pt',
                  context=ITeam)
     def team_view(self):
-
         return dict(
-            heading=self.context.title,
+            heading=self.context.title + ' Roster',
             team=self.context,
             players=self.context.players(),
             coaches=self.context.coaches(),
@@ -44,35 +43,11 @@ class SplashView(Layout):
                  name='tournaments',
                  context=ITeam)
     def tournaments_view(self):
-
         # Hard-wire the tourney names for each team
         team_name = self.context.title
         tourney_names = None
 
         # TODO Generalize this
-        if team_name in ('Blue', 'Orange'):
-            tourney_names = ('Southern Lacrosse', 'Nations Cup',
-                'Beach Blast Tourney', 'Beach Blast Camp',
-                'Capital Cup')
-        elif team_name in ('White', 'Silver'):
-            tourney_names = ('Southern Lacrosse', 'Rock the Field',
-                             'Beach Blast Tourney', 'Beach Blast Camp')
-        elif team_name in ('Black'):
-            tourney_names = ('Southern Lacrosse', 'Sun and Surf')
-
-        return dict(
-            heading='Tournaments',
-            players = self.context.players(),
-            tourney_names = tourney_names
-        )
-
-    @view_config(renderer='templates/team_contacts.pt',
-                 name='contact_info',
-                 context=ITeam)
-    def contacts_view(self):
-
-        # Hard-wire the tourney names for each team
-        team_name = self.context.title
         if team_name in ('Blue', 'Orange'):
             tourney_names = ('Southern Lacrosse', 'Nations Cup',
                              'Beach Blast Tourney', 'Beach Blast Camp',
@@ -84,7 +59,41 @@ class SplashView(Layout):
             tourney_names = ('Southern Lacrosse', 'Sun and Surf')
 
         return dict(
-            heading='Tournaments',
-            players = self.context.players(),
-            tourney_names = tourney_names
+            heading=self.context.title + ' Tournaments',
+            players=self.context.players(),
+            tourney_names=tourney_names
+        )
+
+    @view_config(renderer='templates/team_contacts.pt',
+                 name='contact_info',
+                 context=ITeam)
+    def contacts_view(self):
+        # Hard-wire the tourney names for each team
+        team_name = self.context.title
+
+        return dict(
+            heading=self.context.title + ' Contacts',
+            players=self.context.players(),
+            )
+
+    @view_config(renderer='templates/team_emails.pt',
+                 name='emails',
+                 context=ITeam)
+    def emails_view(self):
+        # Hard-wire the tourney names for each team
+        team_name = self.context.title
+
+        all_emails = set()
+        players = self.context.players()
+
+        for p in players:
+            for email in p.all_emails():
+                all_emails.add(email)
+
+        joined_comma = ', '.join(all_emails)
+
+        return dict(
+            heading=self.context.title + ' Emails',
+            players=players,
+            joined_comma=joined_comma
         )
