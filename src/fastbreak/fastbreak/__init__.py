3c4cd71fdc9c8c4b46ba84ea8051c4568ec4d651
Balazs Ree <ree@greenfinity.hu>
2012-10-02 15:00:00 +0200

    Attempt to stage a /griddemo page. ... /static does not work though, yet.

--- src/fastbreak/fastbreak/__init__.py
+++ src/fastbreak/fastbreak/__init__.py
@@ -4,6 +4,7 @@ from substanced import root_factory
 
 def main(global_config, **settings):
     config = Configurator(settings=settings, root_factory=root_factory)
+    config.add_static_view('static', 'fastbreak:static')
     config.include('substanced')
     config.scan()
     return config.make_wsgi_app()
--- src/fastbreak/fastbreak/views.py
+++ src/fastbreak/fastbreak/views.py
@@ -3,3 +3,7 @@ from pyramid.view import view_config
 @view_config(renderer="templates/hello.pt")
 def hello_view(request):
     return {"tutorial": "Little Dummy"}
+
+@view_config(name="griddemo", renderer="templates/demo76.pt")
+def griddemo_view(request):
+    return {}
